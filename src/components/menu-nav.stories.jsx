74d7a47233cb20c66b56dcbd68a0f0a9056a999c
Raquel Marie <raqueldelamer@gmail.com>
2024-11-11 15:08:16 -0600

    create components, contact.js, update globals.css

---
+++ src/components/menu-nav.stories.jsx
@@ -0,0 +1,13 @@
+import MenuNav from './menu-nav';
+
+const meta = {
+  component: MenuNav,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    menuItems: ["HOME", "ABOUT", "CONTACT"]
+  }
+};
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,1 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import MenuNav from '../components/menu-nav';
+
+export default function HomePage() {
+    const divStyle = {
+        backgroundImage: `url(imgs/capstone-tarot-bg.jpg)`,
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center',
+        height: '100vh', 
+        color: 'black', 
+        text: 'yellow',
+
+    };
+
+    return (
+        <div style={divStyle}>
+        <div className= "text-yellow-500 py-2 px-10 justify-stretch">
+        <MenuNav menuItems={["HOME",  "ABOUT", "CONTACT"]} />
+        <h1>WELCOME TO MY CAPSTONE TAROT APP!</h1>
+        </div>
+        </div>
+    );
+}

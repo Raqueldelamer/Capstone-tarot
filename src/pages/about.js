7199991cf950d1faf86faa6d1029e46fce28b334
Raquel Marie <raqueldelamer@gmail.com>
2024-11-20 14:59:04 -0600

    update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,14 +24,14 @@ export default function AboutPage() {
             <div className="justify-self-center ml-auto mr-auto max-w-5xl text-yellow-200 text-lg font-mono items-center round opacity-90 px-auto mt-25 text-wrap bg-black p-4 rounded shadow-lg mt-4">
                 <div className="card">
     
-                <p>For my Tarot Capstone project, I designed an interactive tarot card app that features a one-card spread, 
+                <p>For my Tarot Capstone project, I designed an interactive tarot card app that features a one-card spread on the Home page, 
                     an About page, and a Contact page with a form that sends email messages via a secure server API endpoint. The projects 
-                    main goal is to provide users with a seamless and engaging tarot reading experience while also allowing them to reach 
+                    main goal is to provide users with a seamless and engaging tarot reading experience while also allowing the user to reach 
                     out through the contact form and a hyperlink to my linkedin profile.
                     <br />
                     <br />
                     I began the project by creating a divStyle that set the apps background image, creating the navigation and header components,
-                    and creating a tarot-data.json file, that contains detailed information about each of 
+                    and compiling a tarot-data.json file, that contains detailed information about each of 
                     the 22 major arcana cards from the Ryder-Waite tarot deck. Since the tarot API I initially planned to use 
                     did not offer image URLs, I decided to build this file myself, including both tarot card images and edited brief 
                     yet insightful card interpretations of each of the cards.  
@@ -45,8 +45,8 @@ export default function AboutPage() {
                     <br />
                     <br />
                     To enhance the visual appeal and interactivity, I utilized Tailwind CSS and Storybook to style key components of the app, such 
-                    as the navigation menu, the fetch button, the headers, a customized Tarot App Icon Social Media Icon hyperlink that I created with iconifyc design.
-                    The result is a modern, responsive design that is both aesthetically pleasing and easy to navigate.
+                    as the navigation menu, the fetch button, the headers, and two customized icons using iconifyc design; the Tarot App Icon 
+                    and a Social Media Icon hyperlink. The result is a modern, responsive design that is both aesthetically pleasing and easy to navigate.
                 </p>
                 </div>
             </div>

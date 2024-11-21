0bb4066c744a20462d8b8cdb2ca6377347a58fbd
Raquel Marie <raqueldelamer@gmail.com>
2024-11-21 14:24:17 -0600

    update Link href to index

--- src/components/menu-nav.js
+++ src/components/menu-nav.js
@@ -8,7 +8,7 @@ export default function MenuNav(props) {
     return (
 
     <div className=" text-yellow-500 text-center font-sans text-xl space-x-6 flex justify-end">
-        <div><Link href="/home" className='hover:text-yellow-200'>{menuItems[0]}</Link></div>
+        <div><Link href="/index" className='hover:text-yellow-200'>{menuItems[0]}</Link></div>
         <div><Link href="/about" className='hover:text-yellow-200'>{menuItems[1]}</Link></div>
         <div><Link href="/contact" className='hover:text-yellow-200'>{menuItems[2]}</Link></div>
     </div>
